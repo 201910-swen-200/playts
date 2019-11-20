6937d785865e29d8b3e26df14b6e7682ccf599ce
Steve Spicklemire <steve@spvi.com>
2019-11-20 08:07:31 -0500

    add node types

--- play.ts
+++ play.ts
@@ -1,7 +1,31 @@
 let x = 3
 
-function foo(value: number): number {
-    return x+3
+class Foo {
+    public a:number
+
+    constructor () {
+        this.a = 0
+    }
+
+    public x(): Foo {
+        this.a += 1
+        return this
+    }
+
+    public y(): Foo {
+        this.a += 2
+        return this
+    }
+
+    public z(): Foo {
+        this.a += 3
+        return this
+    }
 }
 
-console.log(foo(x))
+let b = new Foo()
+
+console.log(b.x().y().a)
+
+
+
